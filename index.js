31691ea8acbfab356a40c9cff95b80dc4d377160
Miko <94553977+gitMiko0@users.noreply.github.com>
2026-05-05 13:49:55 -0600

    updated backend origin

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://artapp-6p6m.onrender.com",
-  "https://elysiana-backend.up.railway.app",
   "https://elysiana.vercel.app",
+  "https://elysianabackend.vercel.app/"
 ];
 
 // Enable CORS for all routes
