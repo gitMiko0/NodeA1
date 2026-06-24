65abc45625aa744255f95ccadc91e0455e5d39c9
gitMiko0 <dustinbengo@gmail.com>
2026-06-24 14:51:20 -0600

    remade supabase

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 // models/db.js - Supabase Client Setup
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qgofaivdyuyxmwgxhebm.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnb2ZhaXZkeXV5eG13Z3hoZWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMTk4NDYsImV4cCI6MjA1NDY5NTg0Nn0.W4gImKc_7lM9gQKaA5yCteVqT2phWk0fzcbmnQ3yEaU";
+const SUPABASE_URL = "https://oywollvzjcqwhtcndinb.supabase.co";
+const SUPABASE_KEY = "sb_publishable__wmxmFaPDLMohi8CfA5bwg_jwNiOGMS";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabase;
